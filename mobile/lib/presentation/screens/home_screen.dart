700d6999ff57633c2d8a40ff71c04a267852cb94
Lance Kian F. Flores <lafa.flores.up@phinmaed.com>
2024-09-10 22:18:22 +0800

    Created our app theme

--- mobile/lib/presentation/screens/home_screen.dart
+++ mobile/lib/presentation/screens/home_screen.dart
@@ -8,6 +8,23 @@ class HomeScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text("HomeScreen"),
       ),
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: (index) {},
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.star),
+            label: "Favorites",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: "Profile",
+          ),
+        ],
+      ),
     );
   }
 }
