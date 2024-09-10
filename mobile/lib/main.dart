98b1af080ae5595d1c395fb7ba1d2e30dbe622e0
Lance Kian F. Flores <lafa.flores.up@phinmaed.com>
2024-09-10 22:52:47 +0800

    Added tab_screen and stack widget in home_screen

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -1,20 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:hotel_flutter/presentation/screens/splash_screen.dart';
 import 'package:hotel_flutter/router/app_router.dart';
-//import 'package:hotel_flutter/screens/welcome_screen.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-// var kColorScheme =
-//     ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 248, 248, 248));
-
 class MyApp extends StatelessWidget {
   final AppRouter _appRouter = AppRouter();
   MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
