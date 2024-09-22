b213ad8e72690c818a6b3854af51b2f77a9a67de
Christer Dale Montilla Reyes <rchristerdale@gmail.com>
2024-09-22 19:22:18 +0800

    authenticaton added

---
+++ api/routes/web3.js
@@ -0,0 +1,9 @@
+// const express = require('express');
+// const web3Controller = require('../controllers/web3');
+// const router = express.Router();
+
+// router
+//   .route('/connect')
+//   .get(web3Controller.connectToMetaMask);
+
+// module.exports = router;
