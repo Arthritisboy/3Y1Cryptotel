c4d5dac2cd8b4f7641abdf10eaf7d5a18180a177
Kawu <138832632+Kawudesu@users.noreply.github.com>
2024-09-23 15:22:04 +0800

    New Update
    Crypto Wallet Page
    Crypto No Transaction Page
    Crypto With Transaction Page

---
+++ mobile/lib/presentation/widgets/cryptowallet/cryptowallet_transactions.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:hotel_flutter/presentation/screens/home_screen.dart';
+import 'package:hotel_flutter/presentation/widgets/cryptowallet/cryptonotransaction.dart';
+import 'package:hotel_flutter/presentation/widgets/cryptowallet/cryptowithtransaction.dart';
+
+class CryptoWalletTransactions extends StatelessWidget {
+  final bool hasTransactions;
+
+  const CryptoWalletTransactions({super.key, this.hasTransactions = false});
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Stack(
+        children: [
+          Container(
+            width: double.infinity,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: const BorderRadius.only(
+                topLeft: Radius.circular(40.0),  // Only top-left corner
+                topRight: Radius.circular(40.0), // Only top-right corner
+              ),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black.withOpacity(0.5),
+                  spreadRadius: 3,
+                  blurRadius: 10,
+                  offset: const Offset(0, 4),
+                ),
+              ],
+            ),
+            padding: const EdgeInsets.all(16.0),
+            child: hasTransactions
+                ? const CryptoWithTransaction()
+                : const CryptoNoTransaction(),
+          ),
+          Positioned(
+            top: 10.0,
+            left: 10.0,
+            child: IconButton(
+              icon: const Icon(Icons.arrow_back, color: Colors.black),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const HomeScreen()),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
