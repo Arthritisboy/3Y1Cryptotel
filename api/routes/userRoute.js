26abf516d969b098019b45e78cf657317b7ead06
Lance Kian F. Flores <lafa.flores.up@phinmaed.com>
2024-09-30 01:52:52 +0800

    deleting their profile

--- api/routes/userRoute.js
+++ api/routes/userRoute.js
@@ -6,6 +6,7 @@ const router = express.Router();
 
 router.route('/').get(authController.protect, userController.getAllUsers);
 router.patch('/updateMe', authController.protect, userController.updateMe);
+router.delete('/deleteMe', authController.protect, userController.deleteMe);
 
 router
   .route('/:id')
