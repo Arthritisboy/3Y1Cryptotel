62dc3642a40ade19ff7bccfe723aa5618b2af12e
Lance Kian F. Flores <lafa.flores.up@phinmaed.com>
2024-09-30 00:05:51 +0800

    Implemented gmail sending token

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -5,6 +5,7 @@ const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/appError');
 const { promisify } = require('util');
 const jwt = require('jsonwebtoken');
+const sendEmail = require('../utils/email');
 
 exports.register = catchAsync(async (req, res) => {
   const newUser = await User.create({ ...req.body });
@@ -93,13 +94,42 @@ exports.restrictTo = (...roles) => {
 };
 
 exports.forgotPassword = catchAsync(async (req, res, next) => {
-  //! 1) Get user based on Posted email
+  //! 1) Get user based on resetToken
   const user = await User.findOne({ email: req.body.email });
   if (!user) next(new AppError('There is no user with email address'), 404);
 
   //! 2) Generate the random reset token
   const resetToken = user.createPasswordResetToken();
   await user.save({ validateBeforeSave: false });
-  //! 3) Send it to user's email
+
+  // ** Node Emailer
+  //! 3) Send it to the user's email
+  const resetURL = `${req.protocol}://${req.get(
+    'host',
+  )}/api/v1/users/resetPassword/${resetToken}`;
+
+  const message = `Forgot your password? Submit a PATCH request with your new password and passwordConfirm to: ${resetURL}.\nIf you didn't forget your password, please ignore this email!`;
+  try {
+    await sendEmail({
+      email: user.email,
+      subject: 'Your password reset token (valid for 10min)',
+      message,
+    });
+    res.status(200).json({
+      status: 'success',
+      message: 'Token sent to email!',
+    });
+  } catch (err) {
+    user.passwordResetToken = undefined;
+    user.passwordResetExpires = undefined;
+    await user.save({ validateBeforeSave: false });
+
+    return next(
+      new AppError(
+        'There was an error sending the email. Try again later!',
+        500,
+      ),
+    );
+  }
 });
 exports.resetPassword = (req, res, next) => {};
--- api/routes/authRoute.js
+++ api/routes/authRoute.js
@@ -6,6 +6,6 @@ const router = express.Router();
 router.route('/signup').post(authController.register);
 router.route('/login').post(authController.login);
 router.post('/forgotPassword', authController.forgotPassword);
-router.post('/resetPassword', authController.resetPassword);
+router.post('/resetPassword/:token', authController.resetPassword);
 
 module.exports = router;
