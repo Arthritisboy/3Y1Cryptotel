b34effd3e3e359d6bedc10cb9beeaf7ea7b3c5ed
Lance Kian F. Flores <lafa.flores.up@phinmaed.com>
2024-10-04 04:16:19 +0800

    Implemented sending verification code

--- api/routes/authRoute.js
+++ api/routes/authRoute.js
@@ -3,10 +3,25 @@ const authController = require('../controllers/authController');
 
 const router = express.Router();
 
+// ** Sign up route
 router.route('/signup').post(authController.register);
+
+// ** Send verification route
+router.post('/sendVerificationCode', authController.sendVerificationCode);
+
+// ** Verify route
+router.post('/verifyCode', authController.verifyCode);
+
+// ** Login route
 router.route('/login').post(authController.login);
+
+// ** Forgot Password route
 router.post('/forgotPassword', authController.forgotPassword);
+
+// ** Reset Password route
 router.patch('/resetPassword/:token', authController.resetPassword);
+
+// * Update Password route
 router.patch(
   '/updateMyPassword',
   authController.protect,
