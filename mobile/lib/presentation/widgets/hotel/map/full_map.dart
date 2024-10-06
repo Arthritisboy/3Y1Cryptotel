756b80ce0d6805b6b0865086ddbaf86bb63d490f
Kawu <138832632+Kawudesu@users.noreply.github.com>
2024-10-06 10:20:40 +0800

    Can open full screen map

---
+++ mobile/lib/presentation/widgets/hotel/map/full_map.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
+
+class FullScreenMap extends StatelessWidget {
+  final String hotelName;
+  final double latitude;
+  final double longitude;
+
+  const FullScreenMap({
+    Key? key,
+    required this.hotelName,
+    required this.latitude,
+    required this.longitude,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(hotelName),
+      ),
+      body: FlutterMap(
+        options: MapOptions(
+          initialCenter: LatLng(latitude, longitude),
+          initialZoom: 15.0,
+        ),
+        children: [
+          TileLayer(
+            urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+            subdomains: ['a', 'b', 'c'],
+            userAgentPackageName: 'com.example.app',
+          ),
+          MarkerLayer(
+            markers: [
+              Marker(
+                point: LatLng(latitude, longitude),
+                width: 80,
+                height: 80,
+                child: Icon(
+                  Icons.location_on,
+                  size: 40,
+                  color: Colors.red,
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
