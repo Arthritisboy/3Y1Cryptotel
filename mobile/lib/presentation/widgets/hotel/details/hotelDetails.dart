081bcb18f80417e5e6c146845635252fc8fa7081
Lance Kian F. Flores <lafa.flores.up@phinmaed.com>
2024-10-08 09:57:53 +0800

    fix activeRoom

--- mobile/lib/presentation/widgets/hotel/details/hotelDetails.dart
+++ mobile/lib/presentation/widgets/hotel/details/hotelDetails.dart
@@ -24,117 +24,120 @@ class HotelDetails extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        const Text(
-          'Details',
-          style: TextStyle(
-            fontSize: 25.0,
-            fontWeight: FontWeight.bold,
-            color: Colors.black,
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Text(
+            'Details',
+            style: TextStyle(
+              fontSize: 25.0,
+              fontWeight: FontWeight.bold,
+              color: Colors.black,
+            ),
           ),
-        ),
-        const SizedBox(height: 10),
-        Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Row(
-              children: [
-                const Icon(Icons.location_on,
-                    color: Color.fromARGB(255, 142, 142, 147), size: 26),
-                const SizedBox(width: 8.0),
-                Expanded(
-                  child: Text(
-                    'Location: $location',
-                    style: const TextStyle(
-                      fontSize: 18.0,
-                      fontWeight: FontWeight.w400,
-                      color: Colors.black,
+          const SizedBox(height: 10),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                children: [
+                  const Icon(Icons.location_on,
+                      color: Color.fromARGB(255, 142, 142, 147), size: 26),
+                  const SizedBox(width: 8.0),
+                  Expanded(
+                    child: Text(
+                      'Location: $location',
+                      style: const TextStyle(
+                        fontSize: 18.0,
+                        fontWeight: FontWeight.w400,
+                        color: Colors.black,
+                      ),
+                      maxLines: 2,
+                      overflow: TextOverflow.visible,
                     ),
-                    maxLines: 2,
-                    overflow: TextOverflow.visible,
+                  ),
+                ],
+              ),
+              const SizedBox(height: 10),
+            ],
+          ),
+          Center(
+            child: MapWidget(latitude: latitude, longitude: longitude),
+          ),
+          const SizedBox(height: 10),
+
+          // Updated TextButton with Icon to the left
+          TextButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => FullScreenMap(
+                    latitude: latitude,
+                    longitude: longitude,
+                    hotelName: hotelName,
+                  ),
+                ),
+              );
+            },
+            child: Row(
+              mainAxisSize: MainAxisSize.min,
+              children: const [
+                Icon(
+                  Icons.map, // Icon moved to the left
+                  color: Color.fromARGB(255, 142, 142, 147),
+                  size: 18,
+                ),
+                SizedBox(width: 5), // Add spacing between the icon and text
+                Text(
+                  'View Map',
+                  style: TextStyle(
+                    color: Colors.blue,
+                    fontSize: 16,
+                    decoration: TextDecoration.underline,
                   ),
                 ),
               ],
             ),
-            const SizedBox(height: 10),
-          ],
-        ),
-        Center(
-          child: MapWidget(latitude: latitude, longitude: longitude),
-        ),
-        const SizedBox(height: 10),
+          ),
 
-        // Updated TextButton with Icon to the left
-        TextButton(
-          onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(
-                builder: (context) => FullScreenMap(
-                  latitude: latitude,
-                  longitude: longitude,
-                  hotelName: hotelName,
-                ),
-              ),
-            );
-          },
-          child: Row(
-            mainAxisSize: MainAxisSize.min,
-            children: const [
-              Icon(
-                Icons.map, // Icon moved to the left
-                color: Color.fromARGB(255, 142, 142, 147),
-                size: 18,
-              ),
-              SizedBox(width: 5), // Add spacing between the icon and text
+          const SizedBox(height: 10),
+          Row(
+            children: [
+              const Icon(Icons.money,
+                  color: Color.fromARGB(255, 142, 142, 147), size: 26),
+              const SizedBox(width: 8.0),
               Text(
-                'View Map',
-                style: TextStyle(
-                  color: Colors.blue,
-                  fontSize: 16,
-                  decoration: TextDecoration.underline,
+                'Price: ₱$price and over',
+                style: const TextStyle(
+                  fontSize: 18.0,
+                  fontWeight: FontWeight.w400,
+                  color: Colors.black,
                 ),
               ),
             ],
           ),
-        ),
-
-        const SizedBox(height: 10),
-        Row(
-          children: [
-            const Icon(Icons.money,
-                color: Color.fromARGB(255, 142, 142, 147), size: 26),
-            const SizedBox(width: 8.0),
-            Text(
-              'Price: ₱$price and over',
-              style: const TextStyle(
-                fontSize: 18.0,
-                fontWeight: FontWeight.w400,
-                color: Colors.black,
+          const SizedBox(height: 10),
+          Row(
+            children: [
+              const Icon(Icons.access_time,
+                  color: Color.fromARGB(255, 142, 142, 147), size: 26),
+              const SizedBox(width: 8.0),
+              Text(
+                'Open Hours: $time',
+                style: const TextStyle(
+                  fontSize: 18.0,
+                  fontWeight: FontWeight.w400,
+                  color: Colors.black,
+                ),
               ),
-            ),
-          ],
-        ),
-        const SizedBox(height: 10),
-        Row(
-          children: [
-            const Icon(Icons.access_time,
-                color: Color.fromARGB(255, 142, 142, 147), size: 26),
-            const SizedBox(width: 8.0),
-            Text(
-              'Open Hours: $time',
-              style: const TextStyle(
-                fontSize: 18.0,
-                fontWeight: FontWeight.w400,
-                color: Colors.black,
-              ),
-            ),
-          ],
-        ),
-        const SizedBox(height: 20),
-      ],
+            ],
+          ),
+          const SizedBox(height: 20),
+        ],
+      ),
     );
   }
 }
