7200286f6630c053a8b9fe917c6676b719c9e7b8
Renek <angjaydeebryann@gmail.com>
2024-10-11 12:25:42 +0800

    fixed authloading state in profile screen

--- mobile/lib/presentation/widgets/profile/blue_background_widget.dart
+++ mobile/lib/presentation/widgets/profile/blue_background_widget.dart
@@ -8,12 +8,7 @@ class BlueBackground extends StatelessWidget {
     return Container(
       color: const Color.fromARGB(255, 29, 53, 115),
       height: MediaQuery.of(context).size.height,
-      child: const Center(
-        child: Text(
-          'Profile',
-          style: TextStyle(color: Colors.white, fontSize: 24),
-        ),
-      ),
+      child: const Center(),
     );
   }
 }
