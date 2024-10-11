076d8c21da4af38e0981c01ec027e9dceb9cdda9
Renek <angjaydeebryann@gmail.com>
2024-10-12 04:06:04 +0800

    75% crypto implementation

--- mobile/lib/presentation/screens/drawerScreens/crypto_wallet.dart
+++ mobile/lib/presentation/screens/drawerScreens/crypto_wallet.dart
@@ -10,19 +10,33 @@ class CryptoWallet extends StatefulWidget {
 }
 
 class _CryptoWalletState extends State<CryptoWallet> {
-  void _selectScreen(String identifier) {
-    Navigator.of(context).pop();
-    print('Selected screen: $identifier');
+  String walletAddress = 'No Address';
+  String balance = '₱ 0';
+  bool isConnected = false;
+
+  void updateWalletInfo(String address, String bal, bool connected) {
+    setState(() {
+      walletAddress = address;
+      balance = bal;
+      isConnected = connected;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Column(
         children: [
-          CryptowalletHeader(),
-          SizedBox(height: 20),
-          CryptoWalletTransactions(),
+          CryptowalletHeader(
+            onWalletUpdated: updateWalletInfo,
+          ),
+          const SizedBox(height: 20),
+          CryptoWalletTransactions(
+            isConnected: isConnected,
+            walletAddress: walletAddress,
+            balance: balance,
+            onWalletUpdated: updateWalletInfo,
+          ),
         ],
       ),
     );
