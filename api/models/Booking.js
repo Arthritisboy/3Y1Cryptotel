6a0e78f6e453fdee7f37136fe0210bed30c35309
Lance Kian F. Flores <lafa.flores.up@phinmaed.com>
2024-10-11 11:32:18 +0800

    modify the booking model

--- api/models/Booking.js
+++ api/models/Booking.js
@@ -1,53 +1,84 @@
 const mongoose = require('mongoose');
 
 const bookingSchema = new mongoose.Schema({
-    bookingType: {
-        type: String,
-        required: true,
-        enum: ['HotelBooking', 'RestaurantBooking'], // Specify allowed types
+  bookingType: {
+    type: String,
+    required: true,
+    enum: ['HotelBooking', 'RestaurantBooking'], // Specify allowed types
+  },
+  hotelId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Hotel',
+  },
+  roomId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Room',
+  },
+  restaurantId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Restaurant',
+  },
+  fullName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  phoneNumber: {
+    type: String,
+    required: true,
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+  tableNumber: {
+    type: Number,
+    validate: {
+      validator: function (value) {
+        // Only validate if bookingType is 'RestaurantBooking'
+        return this.bookingType === 'RestaurantBooking' ? value != null : true;
+      },
+      message: 'Table number is required for Restaurant Booking.',
     },
-    hotelId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Hotel',
-    },
-    roomId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Room',
-    },
-    restaurantId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Restaurant',
-    },
-    tableNumber: {
-        type: Number,
-        validate: {
-            validator: function(value) {
-                // Only validate if bookingType is 'RestaurantBooking'
-                return this.bookingType === 'RestaurantBooking' ? value != null : true;
-            },
-            message: 'Table number is required for Restaurant Booking.',
-        },
-    },
-    checkInDate: {
-        type: Date,
-        required: true,
-    },
-    checkOutDate: {
-        type: Date,
-        required: true,
-    },
-    totalPrice: {
-        type: Number,
-    },
-    status: {
-        type: String,
-        enum: ['pending', 'confirmed', 'cancelled', 'rejected'],
-        default: 'pending',
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now,
-    },
+  },
+  checkInDate: {
+    type: Date,
+    required: true,
+  },
+  checkOutDate: {
+    type: Date,
+    required: true,
+  },
+  timeOfArrival: {
+    type: Date,
+    required: true,
+  },
+  timeOfDeparture: {
+    type: Date,
+  },
+  totalPrice: {
+    type: Number,
+  },
+  adult: {
+    type: Number,
+    required: true,
+  },
+  children: {
+    type: Number,
+    required: true,
+  },
+  status: {
+    type: String,
+    enum: ['pending', 'confirmed', 'cancelled', 'rejected'],
+    default: 'pending',
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
 // Create the Booking model
