c8f9b01b40a5a403d362c4e6c7583de1ed49ded8
Renek <angjaydeebryann@gmail.com>
2024-10-13 01:27:39 +0800

    Revert "update on booking controller and schema"
    This reverts commit 11d70ec06243be33fa9b3e65023a1833e6ce3056.

--- api/models/Booking.js
+++ api/models/Booking.js
@@ -1,96 +1,96 @@
 const mongoose = require('mongoose');
 
 const bookingSchema = new mongoose.Schema({
-    bookingType: {
-        type: String,
-        required: true,
-        enum: ['HotelBooking', 'RestaurantBooking'], // Specify allowed types
+  bookingType: {
+    type: String,
+    required: true,
+    enum: ['HotelBooking', 'RestaurantBooking'], // Specify allowed types
+  },
+  restaurantName: {
+    type: String,
+  },
+  hotelName: {
+    type: String,
+  },
+  roomName: {
+    type: String,
+  },
+  userId: {
+    type: String,
+  },
+  hotelId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Hotel',
+  },
+  roomId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Room',
+  },
+  restaurantId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Restaurant',
+  },
+  fullName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  phoneNumber: {
+    type: String,
+    required: true,
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+  tableNumber: {
+    type: Number,
+    validate: {
+      validator: function (value) {
+        // Only validate if bookingType is 'RestaurantBooking'
+        return this.bookingType === 'RestaurantBooking' ? value != null : true;
+      },
+      message: 'Table number is required for Restaurant Booking.',
     },
-    restaurantName: {
-        type: String,
-    },
-    hotelName: {
-        type: String,
-    },
-    roomName: {
-        type: String,
-    },
-    userId: {
-        type: String,
-    },
-    hotelId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Hotel',
-    },
-    roomId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Room',
-    },
-    restaurantId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Restaurant',
-    },
-    fullName: {
-        type: String,
-        required: true,
-    },
-    email: {
-        type: String,
-        required: true,
-    },
-    phoneNumber: {
-        type: String,
-        required: true,
-    },
-    address: {
-        type: String,
-        required: true,
-    },
-    tableNumber: {
-        type: Number,
-        validate: {
-            validator: function(value) {
-                // Only validate if bookingType is 'RestaurantBooking'
-                return this.bookingType === 'RestaurantBooking' ? value != null : true;
-            },
-            message: 'Table number is required for Restaurant Booking.',
-        },
-    },
-    checkInDate: {
-        type: Date,
-        required: true,
-    },
-    checkOutDate: {
-        type: Date,
-        required: true,
-    },
-    timeOfArrival: {
-        type: Date,
-        required: true,
-    },
-    timeOfDeparture: {
-        type: Date,
-    },
-    totalPrice: {
-        type: Number,
-    },
-    adult: {
-        type: Number,
-        required: true,
-    },
-    children: {
-        type: Number,
-        required: true,
-    },
-    status: {
-        type: String,
-        enum: ['pending', 'accepted', 'cancelled', 'rejected'],
-        default: 'pending',
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now,
-    },
+  },
+  checkInDate: {
+    type: Date,
+    required: true,
+  },
+  checkOutDate: {
+    type: Date,
+    required: true,
+  },
+  timeOfArrival: {
+    type: Date,
+    required: true,
+  },
+  timeOfDeparture: {
+    type: Date,
+  },
+  totalPrice: {
+    type: Number,
+  },
+  adult: {
+    type: Number,
+    required: true,
+  },
+  children: {
+    type: Number,
+    required: true,
+  },
+  status: {
+    type: String,
+    enum: ['pending', 'confirmed', 'cancelled', 'rejected'],
+    default: 'pending',
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
 // Create the Booking model
