a82a5741a481ffda1236f415ecaefadd88a9aa1d
Renek <angjaydeebryann@gmail.com>
2024-10-18 14:49:36 +0800

    changed roles and fixed profile input

--- mobile/lib/presentation/screens/authScreens/login_screen.dart
+++ mobile/lib/presentation/screens/authScreens/login_screen.dart
@@ -38,7 +38,7 @@ class _LoginScreenState extends State<LoginScreen> {
 
               if (!state.hasCompletedOnboarding) {
                 Navigator.pushReplacementNamed(context, '/onboarding');
-              } else if (state.roles == 'admin') {
+              } else if (state.roles == 'manager') {
                 Navigator.pushReplacementNamed(context, '/admin');
               } else {
                 Navigator.pushReplacementNamed(context, '/homescreen');
