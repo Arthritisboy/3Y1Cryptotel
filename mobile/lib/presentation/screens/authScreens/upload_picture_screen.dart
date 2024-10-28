b0e368e0176136064363bac8b8eb72095a95732f
Lance Kian F. Flores <lafa.flores.up@phinmaed.com>
2024-10-29 00:39:56 +0800

    update on error handling

--- mobile/lib/presentation/screens/authScreens/upload_picture_screen.dart
+++ mobile/lib/presentation/screens/authScreens/upload_picture_screen.dart
@@ -1,7 +1,7 @@
 import 'dart:io';
-
 import 'package:flutter/material.dart';
 import 'package:hotel_flutter/logic/bloc/auth/auth_event.dart';
+import 'package:hotel_flutter/logic/bloc/auth/auth_state.dart';
 import 'package:hotel_flutter/presentation/widgets/utils_widget/custom_dialog.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:hotel_flutter/data/model/auth/signup_model.dart';
@@ -27,6 +27,7 @@ class UploadPictureScreen extends StatefulWidget {
   final String confirmPassword;
   final String phoneNumber;
   final String gender;
+
   @override
   State<UploadPictureScreen> createState() => _UploadPictureScreenState();
 }
@@ -42,121 +43,141 @@ class _UploadPictureScreenState extends State<UploadPictureScreen> {
     double screenHeight = MediaQuery.of(context).size.height;
     double screenWidth = MediaQuery.of(context).size.width;
 
-    return Scaffold(
-      body: Column(
-        children: [
-          Expanded(
-            child: SingleChildScrollView(
-              child: Container(
-                color: Colors.white,
-                child: Center(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: [
-                      SizedBox(height: screenHeight * 0.08),
-                      Text(
-                        'CRYPTOTEL',
-                        style: TextStyle(
-                          color: const Color.fromARGB(255, 29, 53, 115),
-                          fontSize: screenHeight * 0.03,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      SizedBox(height: screenHeight * 0.25),
-                      Padding(
-                        padding:
-                            EdgeInsets.symmetric(horizontal: screenWidth * 0.1),
-                        child: Text(
-                          'Please upload your profile picture.',
+    return BlocListener<AuthBloc, AuthState>(
+      listener: (context, state) async {
+        if (state is AuthLoading) {
+          setState(() {
+            _isSigningUp = true; // Show loading state
+          });
+        } else if (state is AuthSuccess) {
+          setState(() {
+            _isSigningUp = false; // Hide loading state
+          });
+
+          // Navigate to verification screen after successful sign-up
+          Navigator.of(context).pushReplacementNamed(
+            '/verifyCode',
+            arguments: {'email': widget.email},
+          );
+        } else if (state is AuthError) {
+          setState(() {
+            _isSigningUp = false; // Hide loading state on error
+          });
+
+          // Show error dialog on failure
+          _showErrorDialog(context, state.error);
+        }
+      },
+      child: Scaffold(
+        body: Column(
+          children: [
+            Expanded(
+              child: SingleChildScrollView(
+                child: Container(
+                  color: Colors.white,
+                  child: Center(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        SizedBox(height: screenHeight * 0.08),
+                        Text(
+                          'CRYPTOTEL',
                           style: TextStyle(
-                            color: Colors.black,
-                            fontSize: screenHeight * 0.018,
-                            fontWeight: FontWeight.w300,
+                            color: const Color.fromARGB(255, 29, 53, 115),
+                            fontSize: screenHeight * 0.03,
+                            fontWeight: FontWeight.bold,
                           ),
-                          textAlign: TextAlign.center,
                         ),
-                      ),
-                      SizedBox(height: screenHeight * 0.05),
-                      _buildProfilePicture(screenHeight),
-                      SizedBox(height: screenHeight * 0.03),
-                      SizedBox(
-                        width: screenWidth * 0.8,
-                        height: screenHeight * 0.07,
-                        child: ElevatedButton(
-                          style: ElevatedButton.styleFrom(
-                            backgroundColor:
-                                const Color.fromARGB(255, 29, 53, 115),
+                        SizedBox(height: screenHeight * 0.25),
+                        Padding(
+                          padding: EdgeInsets.symmetric(
+                              horizontal: screenWidth * 0.1),
+                          child: Text(
+                            'Please upload your profile picture.',
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontSize: screenHeight * 0.018,
+                              fontWeight: FontWeight.w300,
+                            ),
+                            textAlign: TextAlign.center,
                           ),
-                          onPressed: _isLoading
-                              ? null
-                              : () async {
-                                  _pickImage();
-                                },
-                          child: _isLoading
-                              ? const SizedBox(
-                                  height: 20,
-                                  width: 20,
-                                  child: CircularProgressIndicator(
-                                    color: Colors.white,
-                                    strokeWidth: 2,
+                        ),
+                        SizedBox(height: screenHeight * 0.05),
+                        _buildProfilePicture(screenHeight),
+                        SizedBox(height: screenHeight * 0.03),
+                        SizedBox(
+                          width: screenWidth * 0.8,
+                          height: screenHeight * 0.07,
+                          child: ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor:
+                                  const Color.fromARGB(255, 29, 53, 115),
+                            ),
+                            onPressed: _isLoading ? null : _pickImage,
+                            child: _isLoading
+                                ? const SizedBox(
+                                    height: 20,
+                                    width: 20,
+                                    child: CircularProgressIndicator(
+                                      color: Colors.white,
+                                      strokeWidth: 2,
+                                    ),
+                                  )
+                                : const Text(
+                                    'Upload Picture',
+                                    style: TextStyle(fontSize: 18),
                                   ),
-                                )
-                              : const Text(
-                                  'Upload Picture',
-                                  style: TextStyle(fontSize: 18),
-                                ),
+                          ),
                         ),
-                      ),
-                      SizedBox(height: screenHeight * 0.03),
-                    ],
+                        SizedBox(height: screenHeight * 0.03),
+                      ],
+                    ),
                   ),
                 ),
               ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                TextButton(
-                  onPressed: () {
-                    // Trigger sign-up with fallback picture
-                    _signUpUser(context, null); // No image path for "Skip"
-                  },
-                  child: const Text(
-                    'Skip',
-                    style: TextStyle(fontSize: 18, color: Colors.grey),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  TextButton(
+                    onPressed: () {
+                      _signUpUser(context, null);
+                    },
+                    child: const Text(
+                      'Skip',
+                      style: TextStyle(fontSize: 18, color: Colors.grey),
+                    ),
                   ),
-                ),
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: const Color.fromARGB(255, 29, 53, 115),
-                  ),
-                  onPressed: _isSigningUp
-                      ? null // Disable button while signing up
-                      : () {
-                          // Trigger sign-up with the selected profile picture
-                          _signUpUser(context, _selectedImage?.path);
-                        },
-                  child: _isSigningUp
-                      ? const SizedBox(
-                          height: 20,
-                          width: 20,
-                          child: CircularProgressIndicator(
-                            color: Colors.white,
-                            strokeWidth: 2,
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: const Color.fromARGB(255, 29, 53, 115),
+                    ),
+                    onPressed: _isSigningUp
+                        ? null
+                        : () {
+                            _signUpUser(context, _selectedImage?.path);
+                          },
+                    child: _isSigningUp
+                        ? const SizedBox(
+                            height: 20,
+                            width: 20,
+                            child: CircularProgressIndicator(
+                              color: Colors.white,
+                              strokeWidth: 2,
+                            ),
+                          )
+                        : const Text(
+                            'Finish',
+                            style: TextStyle(fontSize: 18),
                           ),
-                        )
-                      : const Text(
-                          'Finish',
-                          style: TextStyle(fontSize: 18),
-                        ),
-                ),
-              ],
+                  ),
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
@@ -201,7 +222,6 @@ class _UploadPictureScreenState extends State<UploadPictureScreen> {
         });
       }
     } catch (e) {
-      // ignore: use_build_context_synchronously
       ScaffoldMessenger.of(context).showSnackBar(
         SnackBar(content: Text('Error: $e')),
       );
@@ -211,10 +231,9 @@ class _UploadPictureScreenState extends State<UploadPictureScreen> {
     });
   }
 
-  // Handle sign-up logic in this function
   void _signUpUser(BuildContext context, String? profilePath) async {
     setState(() {
-      _isSigningUp = true; // Show loading state for the "Finish" button
+      _isSigningUp = true;
     });
 
     final signUpModel = SignUpModel(
@@ -229,30 +248,6 @@ class _UploadPictureScreenState extends State<UploadPictureScreen> {
     );
 
     context.read<AuthBloc>().add(SignUpEvent(signUpModel, profilePath));
-
-    // Wait for the sign-up process to start
-    await Future.delayed(const Duration(milliseconds: 500));
-
-    // ignore: use_build_context_synchronously
-    ScaffoldMessenger.of(context).showSnackBar(
-      const SnackBar(
-        content: Text('Verification code is being sent...'),
-        backgroundColor: Colors.green,
-      ),
-    );
-
-    // Simulate additional loading delay for UX purposes
-    await Future.delayed(const Duration(seconds: 2));
-
-    // ignore: use_build_context_synchronously
-    Navigator.of(context).pushReplacementNamed(
-      '/verifyCode',
-      arguments: {'email': widget.email},
-    );
-
-    setState(() {
-      _isSigningUp = false; // Hide loading state after sign-up is complete
-    });
   }
 
   void _showErrorDialog(BuildContext context, String errorMessage) {
@@ -265,11 +260,12 @@ class _UploadPictureScreenState extends State<UploadPictureScreen> {
           buttonText: 'Retry',
           secondButtonText: 'Cancel',
           onButtonPressed: () {
+            Navigator.of(context).pop(); // Close the dialog
             Navigator.of(context)
-                .pop(); // Close dialog and retry logic if needed
+                .pushReplacementNamed('/signup'); // Navigate to /signup
           },
           onSecondButtonPressed: () {
-            Navigator.of(context).pop(); // Just close the dialog
+            Navigator.of(context).pop(); // Close the dialog
           },
         );
       },
